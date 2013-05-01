3a5cd891b4665876ded5667e76abb7ee1a29c5f6
Matthew Chan <mattkc7@gmail.com>
2013-05-01 01:54:37 -0700

    Refine rspec test cases 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,61 +1,30 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
   
   describe "Home page" do
-    
-    it "should have the h1 'Life Buoy'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Life Buoy')
-    end
-    
-    it "should have the right base title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Life Buoy")
-    end
-    
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    before { visit root_path }
+    it { should have_selector('h1',    text: 'Life Buoy') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
   end
   
   describe "Help page" do
-    
-    it "should have the h1 'Help'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
-    
-    it "should have the right base title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "Life Buoy")
-    end
-    
-    it "should not have a custom page title" do
-          visit '/static_pages/help'
-          page.should_not have_selector('title', :text => '| Help')
-        end
+    before { visit help_path }
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('')) }
   end
   
   describe "About page" do
-    
-    it "should have the h1 'About Us'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
-    
-    it "should have the right base title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "Life Buoy")
-    end
-    
-    it "should not have a custom page title" do
-      visit '/static_pages/about'
-      page.should_not have_selector('title', :text => '| About Us')
-    end
+    before { visit about_path }
+    it { should have_selector('h1',    text: 'About') }
+    it { should have_selector('title', text: full_title('')) }
   end
+  
+  describe "Contact page" do
+      before { visit contact_path }
+      it { should have_selector('h1',    text: 'Contact') }
+      it { should have_selector('title', text: full_title('Contact')) }
+    end
 end
